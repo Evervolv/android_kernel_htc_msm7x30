e356707462d25ed231d19a54c4aba9fc856b3e3c
jdkoreclipse <jdkoreclipse@gmail.com>
2011-07-19 19:53:27 -0400

    bcm4329: backport latest wifi driver from google's 3.0_rc7 kernel

--- drivers/net/wireless/bcm4329/wl_iw.h
+++ drivers/net/wireless/bcm4329/wl_iw.h
@@ -208,7 +208,6 @@ extern int net_os_wake_lock_timeout_enable(struct net_device *dev);
 extern int net_os_set_suspend_disable(struct net_device *dev, int val);
 extern int net_os_set_suspend(struct net_device *dev, int val);
 extern int net_os_set_dtim_skip(struct net_device *dev, int val);
-extern int net_os_set_packet_filter(struct net_device *dev, int val);
 extern void get_customized_country_code(char *country_iso_code, wl_country_t *cspec);
 extern char *dhd_bus_country_get(struct net_device *dev);
 extern int dhd_get_dtim_skip(dhd_pub_t *dhd);
