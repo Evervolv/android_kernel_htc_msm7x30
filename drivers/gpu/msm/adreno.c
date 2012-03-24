ac26d1bc42a71d65a3b8d2a0e68eca12cbb0a251
Jie W <cn.fyodor@gmail.com>
2012-03-24 17:04:07 +0800

    codes clean up again

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -32,13 +32,6 @@
 #include "a2xx_reg.h"
 #include "kgsl_mmu.h"
 
-#define cpu_is_msm7x01()	0
-#define cpu_is_msm7x30()	1
-#define cpu_is_qsd8x50()	0
-#define cpu_is_msm8x60()	0
-#define cpu_is_msm8960()	0
-#define cpu_is_msm8930()	0
-
 #define DRIVER_VERSION_MAJOR   3
 #define DRIVER_VERSION_MINOR   1
 
